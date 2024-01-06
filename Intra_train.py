75fcab8658c6bb553494d5f2a01a4d5c95e5bf3f
wx-cie <43067269+wx-cie@users.noreply.github.com>
2024-01-06 23:01:59 +0800

    Update Intra_train.py

--- Intra_train.py
+++ Intra_train.py
@@ -4,13 +4,15 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parameters to be used for Stacking')
     parser.add_argument('-Name', '--name', required=True, help='input name ')
+    parser.add_argument('-Modelname', '--modelname', required=True, help='input name can be CTISL or MLP ')
     parser.add_argument('-Fileform', '--form', required=True, help='The input file format can be either csv or h5ad ')
     parser.add_argument('-Norm', '--norm', required=True, help='If it is raw data, it needs to be normalized.')
 
     args = parser.parse_args()
     name = args.name
+    modelname=args.modelname
     fileform = args.form
     normalize = args.norm
     feature_num=300
-    dataprocess=CTISL_Intra_Train.DataProcess(name,feature_num,normalize,fileform)
+    dataprocess=CTISL_Intra_Train.DataProcess(name,modelname,feature_num,normalize,fileform)
     dataprocess.split_5k()
