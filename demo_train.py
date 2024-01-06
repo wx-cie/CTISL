65e5d93496fd34d85c62c3ae11fdc7541c06cfff
wx-cie <43067269+wx-cie@users.noreply.github.com>
2024-01-06 23:10:38 +0800

    Update demo_train.py

--- demo_train.py
+++ demo_train.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('-Trainlabel', '--trainlabel', required=True, help='Path to the training dataset labels ')
     parser.add_argument('-Test', '--test', required=True, help='Path to the testing dataset.')
     parser.add_argument('-Predictlabel', '--predictlabel', required=True, help='Path to the predict label.')
+    parser.add_argument('-Modelname', '--form', required=True, help='The input name can be either CTISL or MLP ')
     parser.add_argument('-Fileform', '--form', required=True, help='The input file format can be either csv or h5ad ')
     parser.add_argument('-Norm', '--norm', required=True, help='If it is raw data, it needs to be normalized.')
 
@@ -16,8 +17,9 @@ if __name__ == '__main__':
     trainlabel=args.trainlabel
     testdata=args.test
     predictlabel=args.predictlabel
+    modelname=args.modelname
     fileform = args.form
     normalize = args.norm
     feature_num=300
-    DataProcess.DataProcess(traindata,trainlabel,testdata,predictlabel,feature_num,normalize,fileform).split_train_test()
+    DataProcess.DataProcess(traindata,trainlabel,testdata,predictlabel,modelname, feature_num,normalize,fileform).split_train_test()
 
