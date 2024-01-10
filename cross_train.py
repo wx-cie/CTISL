599ce67d49511a83cfb212545fdd340991873c89
wx-cie <43067269+wx-cie@users.noreply.github.com>
2024-01-10 11:27:31 +0800

    Update cross_train.py

--- cross_train.py
+++ cross_train.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parameters to be used for Stacking')
     parser.add_argument('-Sourcename', '--source', required=True, help='input sourcename ')
     parser.add_argument('-Targetname', '--target', required=True, help='input targetname')
+    parser.add_argument('-ResultPath', '--result', required=True, help='The folder path for the predicted results')
     parser.add_argument('-Modelname', '--modelname', required=True, help='The input name can be either CTISL or MLP')
     parser.add_argument('-Fileform', '--form', required=True, help='The input file format can be either csv or h5ad ')
     parser.add_argument('-Norm', '--norm', required=True, help='If it is raw data, it needs to be normalized.')
@@ -13,6 +14,7 @@ if __name__ == '__main__':
     args =parser.parse_args()
     source_name = args.source
     target_name = args.target
+    result = args.result
     modelname=args.modelname
     fileform =args.form
     normalize=args.norm
@@ -25,5 +27,5 @@ if __name__ == '__main__':
         os.mkdir(data_path)
     dataprocess=CTISL_Inter_Train.DataProcess(name,feature_num,source_name,target_name,normalize,fileform)
     class_num,train,test,trainlabel,testlabel=dataprocess.split_train_test()
-    CTISL_model.Model(name=name, modelname=modelname, class_num=class_num,m_k=1,feature_num=feature_num,traindata=train,
+    CTISL_model.Model(name=name, modelname=modelname, result=result,class_num=class_num,m_k=1,feature_num=feature_num,traindata=train,
                       testdata=test,trainlabel=trainlabel,testlabel=testlabel)
